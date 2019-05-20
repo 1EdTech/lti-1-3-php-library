497af4b20f9114dfcb51bdac42a7ca6274882555
Martin Lenord <mlenord@turnitin.com>
2019-05-20 07:07:33 -0700

    fix forwarded headers

--- src/lti/lti.php
+++ src/lti/lti.php
@@ -2,5 +2,5 @@
 include_once("lti_oidc_login.php");
 include_once("lti_message_launch.php");
 include_once("database.php");
-define("TOOL_HOST", $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
+define("TOOL_HOST", ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?: $_SERVER['REQUEST_SCHEME']) . '://' . $_SERVER['HTTP_HOST']);
 ?>
