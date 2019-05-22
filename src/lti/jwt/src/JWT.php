669efe33e2b2214c8cce26663ad87c2c3ff9467e
Martin Lenord <mlenord@turnitin.com>
2019-05-22 09:41:03 -0700

    add composer for example

--- src/lti/jwt/src/JWT.php
+++ src/lti/jwt/src/JWT.php
@@ -27,7 +27,7 @@ class JWT
      * we want to provide some extra leeway time to
      * account for clock skew.
      */
-    public static $leeway = 0;
+    public static $leeway = 10;
 
     /**
      * Allow the current timestamp to be specified.
