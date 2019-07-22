8af46b4a30ee9040df12cd3852de7acebc41f962
Martin Lenord <mlenord@turnitin.com>
2019-07-22 21:57:52 +0100

    clock skew fix

--- src/lti/lti.php
+++ src/lti/lti.php
@@ -3,4 +3,5 @@ include_once("lti_oidc_login.php");
 include_once("lti_message_launch.php");
 include_once("database.php");
 define("TOOL_HOST", ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?: $_SERVER['REQUEST_SCHEME']) . '://' . $_SERVER['HTTP_HOST']);
+Firebase\JWT\JWT::$leeway = 5;
 ?>
