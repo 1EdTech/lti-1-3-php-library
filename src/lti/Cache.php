e6ffb52e42e720747bd4f8f6c044c48e8210a994
Martin Lenord <mlenord@turnitin.com>
2020-02-07 16:40:29 +0000

    change to using file for cache rather than session

--- src/lti/Cache.php
+++ src/lti/Cache.php
@@ -2,26 +2,45 @@
 namespace IMSGlobal\LTI;
 
 class Cache {
+
+    private $cache;
+
     public function get_launch_data($key) {
-        return $_SESSION[$key];
+        $this->load_cache();
+        return $this->cache[$key];
     }
 
     public function cache_launch_data($key, $jwt_body) {
-        $_SESSION[$key] = $jwt_body;
+        $this->cache[$key] = $jwt_body;
+        $this->save_cache();
         return $this;
     }
 
     public function cache_nonce($nonce) {
-        $_SESSION['nonce'][$nonce] = true;
+        $this->cache['nonce'][$nonce] = true;
+        $this->save_cache();
         return $this;
     }
 
     public function check_nonce($nonce) {
-        if (!isset($_SESSION['nonce'][$nonce])) {
+        $this->load_cache();
+        if (!isset($this->cache['nonce'][$nonce])) {
             return false;
         }
-        unset($_SESSION['nonce'][$nonce]);
         return true;
+    }
+
+    private function load_cache() {
+        $cache = file_get_contents(sys_get_temp_dir() . '/lti_cache.txt');
+        if (empty($cache)) {
+            file_put_contents(sys_get_temp_dir() . '/lti_cache.txt', '{}');
+            $this->cache = [];
+        }
+        $this->cache = json_decode($cache, true);
+    }
+
+    private function save_cache() {
+        file_put_contents(sys_get_temp_dir() . '/lti_cache.txt', json_encode($this->cache));
     }
 }
 ?>
