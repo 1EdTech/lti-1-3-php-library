43ff0b570b7fd83deffe24fc2bc27daf696f7009
Crystal Ophaso <crystal.ophaso@packback.co>
2021-06-21 13:37:47 -0700

    PODA-7: fix test

--- tests/LtiServiceConnectorTest.php
+++ tests/LtiServiceConnectorTest.php
@@ -3,6 +3,8 @@
 namespace Tests;
 
 use Mockery;
+use GuzzleHttp\Client;
+use Packback\Lti1p3\Interfaces\Cache;
 use Packback\Lti1p3\Interfaces\LtiRegistrationInterface;
 use Packback\Lti1p3\LtiServiceConnector;
 use PHPUnit\Framework\TestCase;
@@ -12,8 +14,10 @@ class LtiServiceConnectorTest extends TestCase
     public function testItInstantiates()
     {
         $registration = Mockery::mock(LtiRegistrationInterface::class);
+        $cache = Mockery::mock(Cache::class);
+        $client = Mockery::mock(Client::class);
 
-        $connector = new LtiServiceConnector($registration);
+        $connector = new LtiServiceConnector($registration, $cache, $client);
 
         $this->assertInstanceOf(LtiServiceConnector::class, $connector);
     }
