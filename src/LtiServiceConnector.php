ccb41f84120d870bf052c2f817e5add695caea6a
Crystal Ophaso <crystal.ophaso@packback.co>
2021-06-21 09:51:27 -0700

    PODA-7: use guzzle and camelCase all the things

--- src/LtiServiceConnector.php
+++ src/LtiServiceConnector.php
@@ -32,24 +32,22 @@ class LtiServiceConnector implements LtiServiceConnectorInterface
     {
         // Don't fetch the same key more than once.
         sort($scopes);
-        $scope_key = md5(implode('|', $scopes));
+        $scopeKey = md5(implode('|', $scopes));
 
         // Build up JWT to exchange for an auth token
-        $client_id = $this->registration->getClientId();
+        $clientId = $this->registration->getClientId();
 
         // Store access token with a unique key
-        $accessTokenKey = $scope_key.'-'.$client_id;
+        $accessTokenKey = $scopeKey.'-'.$clientId;
 
-        // TODO: Refresh TOKEN
-        // https://github.com/oat-sa/lib-lti1p3-core/blob/e294e61742782f557f69ddd73202826516cb8f07/src/Service/Client/LtiServiceClient.php#L132
-        // Get Access Token from cache if it exists
+        // Get Access Token from cache if it exists and is not expired.
         if ($this->cache->getAccessToken($accessTokenKey)) {
             return $this->cache->getAccessToken($accessTokenKey);
         }
 
-        $jwt_claim = [
-                "iss" => $client_id,
-                "sub" => $client_id,
+        $jwtClaim = [
+                "iss" => $clientId,
+                "sub" => $clientId,
                 "aud" => $this->registration->getAuthServer(),
                 "iat" => time() - 5,
                 "exp" => time() + 60,
@@ -57,10 +55,10 @@ class LtiServiceConnector implements LtiServiceConnectorInterface
         ];
 
         // Sign the JWT with our private key (given by the platform on registration)
-        $jwt = JWT::encode($jwt_claim, $this->registration->getToolPrivateKey(), 'RS256', $this->registration->getKid());
+        $jwt = JWT::encode($jwtClaim, $this->registration->getToolPrivateKey(), 'RS256', $this->registration->getKid());
 
         // Build auth token request headers
-        $auth_request = [
+        $authRequest = [
             'grant_type' => 'client_credentials',
             'client_assertion_type' => 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
             'client_assertion' => $jwt,
@@ -74,25 +72,19 @@ class LtiServiceConnector implements LtiServiceConnectorInterface
         // Get Access
         $response = $this->client->post($url, [
             'timeout' => 10,
-            'form_params' => $auth_request,
+            'form_params' => $authRequest,
         ]);
 
-        $token_data = json_decode($response->getBody()->__toString(), true);
+        $tokenData = json_decode($response->getBody()->__toString(), true);
 
         // Cache access token
-        $this->cache->cacheAccessToken($accessTokenKey, $token_data['access_token']);
+        $this->cache->cacheAccessToken($accessTokenKey, $tokenData['access_token']);
 
-        return $token_data['access_token'];
+        return $tokenData['access_token'];
     }
 
     public function makeServiceRequest(array $scopes, $method, $url, $body = null, $contentType = 'application/json', $accept = 'application/json')
     {
-        // $ch = curl_init();
-        // $headers = [
-        //     'Authorization: Bearer '.$this->getAccessToken($scopes),
-        //     'Accept:'.$accept,
-        // ];
-
         $headers = [
             'Authorization' => 'Bearer '.$this->getAccessToken($scopes),
             'Accept' => $accept,
@@ -100,72 +92,34 @@ class LtiServiceConnector implements LtiServiceConnectorInterface
 
         $this->client = new Client();
 
-    //     curl_setopt($ch, CURLOPT_URL, $url);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //     curl_setopt($ch, CURLOPT_HEADER, 1);
-    //     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    //     curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-        // if ($method === 'POST') {
-    //         curl_setopt($ch, CURLOPT_POST, 1);
-    //         curl_setopt($ch, CURLOPT_POSTFIELDS, strval($body));
-            // $headers[] = 'Content-Type: '.$contentType;
-        // }
+        try {
+            if ($method === 'POST') {
+                $headers = array_merge($headers, ['Content-Type' => $contentType]);
 
-    //     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    //     $response = curl_exec($ch);
+                $response = $this->client->request($method, $url, [
+                    'headers' => $headers,
+                    'json' => $body,
+                    'timeout' => 60,
+                ]);
+            } else {
+                $response = $this->client->request($method, $url, [
+                    'timeout' => 60,
+                    'headers' => $headers,
+                ]);
+            }
 
-    try {
-        if ($method === 'POST') {
-            $headers = array_merge($headers, ['Content-Type' => $contentType]);
+            $headerSize = $response->getHeader('Content-Length');
 
-            // \Log::info($headers);
-            // \Log::info($method);
-            // \Log::info($body);
+            $respHeaders = substr($response, 0, $headerSize);
+            $respBody = substr($response, $headerSize);
 
-            $response = $this->client->request($method, $url, [
-                'headers' => $headers,
-                'json' => $body,
-                'timeout' => 60,
-                'http_errors' => true
-            ]);
-        } else {
-            $response = $this->client->request($method, $url, [
-                'timeout' => 60,
-                'headers' => $headers,
-            ]);
+            return [
+                'headers' => array_filter(explode("\r\n", $respHeaders)),
+                'body' => json_decode($respBody, true),
+            ];
+
+        } catch (\Exception $exception) {
+            echo 'Request Error:'.$exception->getMessage();
         }
-
-        \Log::info(json_decode($response->getBody()));
-
-        // $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-
-        $header_size = $response->getHeader('Content-Length');
-
-        $resp_headers = substr($response, 0, $header_size);
-        $resp_body = substr($response, $header_size);
-
-        return [
-            'headers' => array_filter(explode("\r\n", $resp_headers)),
-            'body' => json_decode($resp_body, true),
-        ];
-
-    } catch (\Exception $exception) {
-        \Log::error($exception->getMessage());
-        // echo 'Request Error:'.$exception->getMessage();
-    }
-
-    //     if (curl_errno($ch)) {
-    //         echo 'Request Error:'.curl_error($ch);
-    //     }
-    //     $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-    //     curl_close($ch);
-
-    //     $resp_headers = substr($response, 0, $header_size);
-    //     $resp_body = substr($response, $header_size);
-
-    //     return [
-    //         'headers' => array_filter(explode("\r\n", $resp_headers)),
-    //         'body' => json_decode($resp_body, true),
-    //     ];
     }
 }
