c09d1ea6bdfed560b948cf37ca21314678e5cf55
Crystal Ophaso <crystal.ophaso@packback.co>
2021-06-22 14:55:28 -0700

    PODA-7: try to add test for service connectore

--- tests/LtiServiceConnectorTest.php
+++ tests/LtiServiceConnectorTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+// use Firebase\JWT\JWT;
 use GuzzleHttp\Client;
 use Mockery;
 use Packback\Lti1p3\Interfaces\ICache;
@@ -13,11 +14,12 @@ class LtiServiceConnectorTest extends TestCase
 {
     public function setUp(): void
     {
-        $registration = Mockery::mock(ILtiRegistration::class);
-        $cache = Mockery::mock(ICache::class);
-        $client = Mockery::mock(Client::class);
+        // $this->jwt = Mockery::mock(JWT::class);
+        $this->registration = Mockery::mock(ILtiRegistration::class);
+        $this->cache = Mockery::mock(ICache::class);
+        $this->client = Mockery::mock(Client::class);
 
-        $this->connector = new LtiServiceConnector($registration, $cache, $client);
+        $this->connector = new LtiServiceConnector($this->registration, $this->cache, $this->client);
     }
 
     public function testItInstantiates()
@@ -25,7 +27,69 @@ class LtiServiceConnectorTest extends TestCase
         $this->assertInstanceOf(LtiServiceConnector::class, $this->connector);
     }
 
+    public function testItGetsCachedAccessToken()
+    {
+        $this->registration->shouldReceive('getClientId')
+            ->once()
+            ->andReturn('client_id');
+        $this->registration->shouldReceive('getIssuer')
+            ->once()
+            ->andReturn('issuer');
+        $this->cache->shouldReceive('getAccessToken')
+            ->once()
+            ->andReturn('TokenOfAccess');
+
+        $result = $this->connector->getAccessToken(['scopeKey']);
+
+        $this->assertEquals($result, 'TokenOfAccess');
+    }
+
     /*
-     * @todo Finish testing
+     * @todo Figure out how to test this
      */
+    // public function testItGetsAccessToken()
+    // {
+    //     $this->registration->shouldReceive('getClientId')
+    //         ->once()
+    //         ->andReturn('client_id');
+    //     $this->registration->shouldReceive('getIssuer')
+    //         ->once()
+    //         ->andReturn('issuer');
+    //     $this->cache->shouldReceive('getAccessToken')
+    //         ->once()
+    //         ->andReturn();
+    //     $this->registration->shouldReceive('getAuthServer')
+    //         ->once()
+    //         ->andReturn('auth_server');
+    //     $this->registration->shouldReceive('getToolPrivateKey')
+    //         ->once()
+    //         ->andReturn('toolprivatekey');
+    //     $this->registration->shouldReceive('getKid')
+    //         ->once()
+    //         ->andReturn('kid');
+
+        // Error: supplied key param cannot be coerced into a private key
+    //     $this->jwt->shouldReceive('encode')
+    //         ->once()
+    //         ->andReturn('jwt');
+
+
+    //     $this->registration->shouldReceive('getAuthTokenUrl')
+    //         ->once()
+    //         ->andReturn('auth_token_url');
+    //     $this->client->shouldReceive('post')
+    //         ->once()
+    //         ->andReturn([
+    //             'body' => [
+    //                 'access_token' => 'accessToken'
+    //             ]
+    //         ]);
+    //     $this->cache->shouldReceive('cacheAccessToken')
+    //         ->once()
+    //         ->andReturn();
+
+    //     $result = $this->connector->getAccessToken(['scopeKey']);
+
+    //     $this->assertEquals($result, 'TokenOfAccess');
+    // }
 }
