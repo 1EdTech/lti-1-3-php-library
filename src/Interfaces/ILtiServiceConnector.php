744f0f4de4543ad05427e3196793c900f705e934
Eric Tendian <erictendian@gmail.com>
2021-07-08 18:23:57 -0500

    DATAINF-97 Change how POST requests are made to fix double-encoded JSON

--- src/Interfaces/ILtiServiceConnector.php
+++ src/Interfaces/ILtiServiceConnector.php
@@ -6,5 +6,5 @@ interface ILtiServiceConnector
 {
     public function getAccessToken(array $scopes);
 
-    public function makeServiceRequest(array $scopes, $method, $url, $body = null, $contentType = 'application/json', $accept = 'application/json');
+    public function makeServiceRequest(array $scopes, string $method, string $url, string $body = null, $contentType = 'application/json', $accept = 'application/json');
 }
