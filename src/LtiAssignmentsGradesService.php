dfcab53d8f9c846685fc640d14cbf77e36ec7034
pb-sobrien <susie@packback.co>
2021-08-12 09:36:12 -0500

    DEV-11392: linting

--- src/LtiAssignmentsGradesService.php
+++ src/LtiAssignmentsGradesService.php
@@ -128,6 +128,7 @@ class LtiAssignmentsGradesService
         if (isset($line_items['body']['id'])) {
             $line_items['body'] = [$line_items['body']];
         }
+
         return $line_items;
     }
 }
